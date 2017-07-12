f367e70dd54194d7fb2c5bb98abbe23f1b4ec016
SalmanSk <mohdsalmanshaikh63@gmail.com>
2017-07-12 19:20:42 +0530

    Adding UtilityClass, StopWatch, InventoryManagerApp, Regex

---
+++ BasicJavaPrograms/src/com/aceprogrammer/basics/InventoryManagerDemo/InventoryManagerApp.java
@@ -0,0 +1,12 @@
+package com.aceprogrammer.basics.InventoryManagerDemo;
+
+public class InventoryManagerApp {
+
+	public static void main(String[] args) {
+		System.out.println("This is a demo of inventory manager app");
+		InventoryManager iManager = new InventoryManager();
+		iManager.displayInventory();
+
+	}
+
+}
